4c29370435abaa517f631c90c3a5ea1697b3d0bf
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-26 23:50:11 +0500

    Add stored procedures for trip table and password retrieval

--- Database Queries/Stored_Procedure.sql
+++ Database Queries/Stored_Procedure.sql
@@ -1,4 +1,26 @@
+SET @image_location = null;
+
+-- Insertion Procedure for trip table
 DELIMITER //
+CREATE PROCEDURE InsertTrip(
+    IN p_trip_name VARCHAR(50),
+    IN p_source_city_id INT,
+    IN p_destination_city_id INT,
+    IN p_start_date DATE,
+    IN p_trip_days INT,
+    IN p_trip_budget INT,
+    IN p_no_of_persons INT
+)
+BEGIN
+    INSERT INTO trip (
+        trip_name, source_city_id, destination_city_id, 
+        start_date, trip_days, trip_budget, no_of_persons
+    ) VALUES (
+        p_trip_name, p_source_city_id, p_destination_city_id,
+        p_start_date, p_trip_days, p_trip_budget, p_no_of_persons
+    );
+END //
+
 CREATE PROCEDURE GetPasswordByEmail
 (IN p_email VARCHAR(40), 
 OUT p_password VARCHAR(20))
@@ -11,11 +33,8 @@ BEGIN
 
     SET p_password = v_password;
 END //
-DELIMITER ;
 
-SET @image_location = null;
 
-DELIMITER //
 CREATE PROCEDURE addLocationImage
 (IN lct_id INT,
 IN image BLOB)
@@ -26,4 +45,6 @@ BEGIN
     VALUES
     (image, 'LOCATION');
 END //
+
+
 DELIMITER ;
