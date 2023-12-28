8888f015b09ba3a7caf225dc8a7fa7973008fd40
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-28 20:45:45 +0500

    Add general stored procedures - Password retrieval - Image insertion

---
+++ Database_Queries/Stored_Procedures/General_Procedures.sql
@@ -0,0 +1,30 @@
+DELIMITER //
+
+-- Insertion Procedure retrieve password by email
+CREATE PROCEDURE GetPasswordByEmail
+(IN p_email VARCHAR(40), 
+OUT p_password VARCHAR(20))
+BEGIN
+    DECLARE v_password VARCHAR(20);
+
+    SELECT password INTO v_password
+    FROM `user`
+    WHERE user_email = p_email;
+
+    SET p_password = v_password;
+END //
+
+
+-- Insertion Procedure to add Image related to Location
+CREATE PROCEDURE addLocationImage
+(IN lct_id INT,
+IN image BLOB)
+BEGIN
+    SET @image_location = lct_id;
+    INSERT INTO image
+    (image_data, image_type)
+    VALUES
+    (image, 'LOCATION');
+END //
+
+DELIMITER ;
