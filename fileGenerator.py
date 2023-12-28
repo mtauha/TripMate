9055277d4fbc98c45834c8655bdc992552fc451a
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-28 23:05:36 +0500

    Fix database connection issue in fileGenerator.py

--- fileGenerator.py
+++ fileGenerator.py
@@ -7,11 +7,23 @@ filename = os.path.join(os.getcwd(), filename)
 
 with open(filename, "w") as f:
     f.write(
-    """<?php
-            
-    include "D:\study\Projects\Kotlin\TripMate\APIs\connection.php";
-            
-            
+        """<?php
+
+include "D:\study\Projects\Kotlin\TripMate\APIs\connection.php";
+
+$arr = [];
+
+$con = dbConnection();
+
+if ($con) {
+    $arr["connection"] = "Success";
+    echo json_encode($arr);
+}
+else{
+    $arr["connection"] = "Failed";
+    echo json_encode($arr);
+    return;
+}
 
 ?>"""
     )
