159a30d8c8ff25b1f7951c176b78891f2fe956b4
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-28 23:56:52 +0500

    Refactor insert_chat_data.php Remove unnecessary variable and update query

--- APIs/Insert/insert_chat_data.php
+++ APIs/Insert/insert_chat_data.php
@@ -17,7 +17,6 @@ else{
 }
 
 // Validate and sanitize the input parameters
-$chat_id = isset($_POST["chat_id"]) ? $_POST["chat_id"] : null;
 $group_id = isset($_POST["group_id"]) ? $_POST["group_id"] : null;
 $user_id = isset($_POST["user_id"]) ? $_POST["user_id"] : null;
 $message = isset($_POST["message"]) ? $_POST["message"] : null;
@@ -25,13 +24,13 @@ $image_id = isset($_POST["image_id"]) ? $_POST["image_id"] : null;
 $chat_time = isset($_POST["chat_time"]) ? $_POST["chat_time"] : null;
 
 // Check if any required parameter is missing
-if (!$chat_id || !$group_id || !$user_id || !$message || !$image_id || !$chat_time) {
+if (!$group_id || !$user_id || !$message || !$image_id || !$chat_time) {
     $arr["success"] = "false";
     echo json_encode($arr);
     return;
 }
 
-$query = "CALL InsertChat('$chat_id', '$group_id', '$user_id', '$message', '$image_id', '$chat_time')";
+$query = "CALL InsertChat('$group_id', '$user_id', '$message', '$image_id', '$chat_time')";
 
 $res = mysqli_query($con, $query);
 if ($res) {
