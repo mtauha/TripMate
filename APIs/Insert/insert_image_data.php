06c28ff870a629f81795b4cff787f22ec83f5166
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-28 23:18:41 +0500

    Add insert_image_data.php Inserting image data into the database

---
+++ APIs/Insert/insert_image_data.php
@@ -0,0 +1,66 @@
+<?php
+
+class ImageType
+{
+    const LOCATION = "LOCATION";
+    const CHAT = "CHAT";
+    const PROFILE_PIC = "PROFILE PICTURE";
+}
+
+
+include "D:\study\Projects\Kotlin\TripMate\APIs\connection.php";
+
+$arr = [];
+
+$con = dbConnection();
+
+if ($con) {
+    $arr["connection"] = "Success";
+    echo json_encode($arr);
+} else {
+    $arr["connection"] = "Failed";
+    echo json_encode($arr);
+    return;
+}
+
+
+
+if (!empty($_POST["image_data"])) {
+    $image_data = $_POST["image_data"];
+} else {
+    $arr["success"] = "false";
+    $arr["error"] = "$image_data incorrect";
+    echo json_encode($arr);
+    return;
+}
+
+if (!empty($_POST["image_type"])) {
+    $image_type = $_POST["image_type"];
+
+    if (!defined("ImageType::$image_type")) {
+        $arr["success"] = "false";
+        $arr["error"] = "'$image_type' image type incorrect";
+        echo json_encode($arr);
+        return;
+    }
+} else {
+    $arr["success"] = "false";
+    $arr["error"] = "$image_type incorrect";
+    echo json_encode($arr);
+    return;
+}
+
+$query = "CALL InsertGroup('$image_data','$image_type')";
+
+$res = mysqli_query($con, $query);
+if ($res) {
+    $arr["success"] = "true";
+    $arr["group_id"] = mysqli_insert_id($con);
+} else {
+    $arr["success"] = "false";
+    $arr["error"] = "Insertion failed";
+}
+
+echo json_encode($arr);
+
+?>
