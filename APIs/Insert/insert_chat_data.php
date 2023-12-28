c3cb6ec19b448bea07da78dbefaf9efb79bca0e3
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-28 22:48:00 +0500

    Refactor insert_chat_data.php Validate and sanitize input parameters

--- APIs/Insert/insert_chat_data.php
+++ APIs/Insert/insert_chat_data.php
@@ -1,72 +1,36 @@
 <?php
-            
+
 include "D:\study\Projects\Kotlin\TripMate\APIs\connection.php";
-            
+
 $arr = [];
 
 $con = dbConnection();
 
+// Validate and sanitize the input parameters
+$chat_id = isset($_POST["chat_id"]) ? $_POST["chat_id"] : null;
+$group_id = isset($_POST["group_id"]) ? $_POST["group_id"] : null;
+$user_id = isset($_POST["user_id"]) ? $_POST["user_id"] : null;
+$message = isset($_POST["message"]) ? $_POST["message"] : null;
+$image_id = isset($_POST["image_id"]) ? $_POST["image_id"] : null;
+$chat_time = isset($_POST["chat_time"]) ? $_POST["chat_time"] : null;
 
-if(isset($_POST["chat_id"])){
-    $chat_id = $_POST["chat_id"];
-}
-else{
+// Check if any required parameter is missing
+if (!$chat_id || !$group_id || !$user_id || !$message || !$image_id || !$chat_time) {
+    $arr["success"] = "false";
+    echo json_encode($arr);
     return;
 }
-
-
-if(isset($_POST["group_id"])){
-    $group_id = $_POST["group_id"];
-}
-else{
-    return;
-}
-
-
-if(isset($_POST["user_id"])){
-    $user_id = $_POST["user_id"];
-}
-else{
-    return;
-}
-
-
-if(isset($_POST["message"])){
-    $message = $_POST["message"];
-}
-else{
-    return;
-}
-
-
-
-if(isset($_POST["image_id"])){
-    $image_id = $_POST["image_id"];
-}
-else{
-    return;
-}
-
-
-if(isset($_POST["chat_time"])){
-    $chat_time = $_POST["chat_time"];
-}
-else{
-    return;
-}
-
 
 $query = "CALL InsertChat('$chat_id', '$group_id', '$user_id', '$message', '$image_id', '$chat_time')";
 
 $res = mysqli_query($con, $query);
-if($res){
+if ($res) {
     $arr["success"] = "true";
     $arr["chat_id"] = mysqli_insert_id($con);
-}
-else{
+} else {
     $arr["success"] = "false";
 }
 
-echo json_encode($arr);    
+echo json_encode($arr);
 
 ?>
