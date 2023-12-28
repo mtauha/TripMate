8f0d67d69d190ed489cac455909d1b0fa0fab069
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-28 21:52:24 +0500

    Update stored procedures
    - Password retrieval
    - Image addition
    - User-to-group relation
    - User removal from group

--- Database_Queries/Stored_Procedures/General_Procedures.sql
+++ Database_Queries/Stored_Procedures/General_Procedures.sql
@@ -1,6 +1,6 @@
 DELIMITER //
 
--- Insertion Procedure retrieve password by email
+--* Insertion Procedure retrieve password by email
 CREATE PROCEDURE GetPasswordByEmail
 (IN p_email VARCHAR(40), 
 OUT p_password VARCHAR(20))
@@ -15,7 +15,7 @@ BEGIN
 END //
 
 
--- Insertion Procedure to add Image related to Location
+--* Insertion Procedure to add Image related to Location
 CREATE PROCEDURE addLocationImage
 (IN lct_id INT,
 IN image BLOB)
@@ -27,4 +27,44 @@ BEGIN
     (image, 'LOCATION');
 END //
 
+
+--* Insertion Procedure to add user to group
+CREATE PROCEDURE AddUserToGroup(
+    IN p_user_id INT,
+    IN p_group_id INT
+)
+BEGIN
+    -- Check if user_id exists in the user table, if not, cancel the insertion
+    IF NOT EXISTS (SELECT * FROM `user` WHERE user_id = p_user_id) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User does not exist';
+    END IF;
+
+    -- Check if group_id exists in the group table, if not, cancel the insertion
+    IF NOT EXISTS (SELECT * FROM `group` WHERE group_id = p_group_id) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Group does not exist';
+    END IF;
+
+    -- Insert data into the user_to_group_relation table
+    INSERT INTO `user_to_group_relation` (user_id, group_id)
+    VALUES (p_user_id, p_group_id);
+END //
+
+
+--* Remove Procedure to remove user from group
+CREATE PROCEDURE RemoveUserFromGroup(
+    IN p_user_id INT,
+    IN p_group_id INT
+)
+BEGIN
+    -- Check if user_id and group_id combination exists in the user_to_group_relation table, if not, cancel the deletion
+    IF NOT EXISTS (
+        SELECT * FROM `user_to_group_relation`
+        WHERE user_id = p_user_id AND group_id = p_group_id
+    ) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User to Group relation does not exist';
+    END IF;
+
+    -- Delete data from the user_to_group_relation table
+    DELETE FROM `user_to_group_relation` WHERE user_id = p_user_id AND group_id = p_group_id;
+END //
 DELIMITER ;
