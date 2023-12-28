8ea13e64edeba2195f9c4568bfbd26db73ae3378
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-28 23:40:09 +0500

    Add insert_picture_to_location_relation_data.php

---
+++ APIs/Insert/insert_picture_to_location_relation_data.php
@@ -0,0 +1,52 @@
+<?php
+
+include "D:\study\Projects\Kotlin\TripMate\APIs\connection.php";
+
+$arr = [];
+
+$con = dbConnection();
+
+if ($con) {
+    $arr["connection"] = "Success";
+    echo json_encode($arr);
+} else {
+    $arr["connection"] = "Failed";
+    echo json_encode($arr);
+    return;
+}
+
+
+
+if (!empty($_POST["location_id"])) {
+    $location_id = $_POST["location_id"];
+} 
+else {
+    $arr["success"] = "false";
+    $arr["error"] = "$location_id incorrect";
+    echo json_encode($arr);
+    return;
+}
+
+if (!empty($_POST["image_data"])) {
+    $image_data = base64_decode($_POST["image_data"]);
+} else {
+    $arr["success"] = "false";
+    $arr["error"] = "$image_data incorrect";
+    echo json_encode($arr);
+    return;
+}
+
+$query = "CALL InsertGroup('$location_id','$image_data')";
+
+$res = mysqli_query($con, $query);
+if ($res) {
+    $arr["success"] = "true";
+    $arr["group_id"] = mysqli_insert_id($con);
+} else {
+    $arr["success"] = "false";
+    $arr["error"] = "Insertion failed";
+}
+
+echo json_encode($arr);
+
+?>
