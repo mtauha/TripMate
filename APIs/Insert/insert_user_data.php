06bd392d692eec17649578656d80f5ee90910a44
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-30 12:05:19 +0500

    Refactor insert_user_data.php To use a function for inserting user data

--- APIs/Insert/insert_user_data.php
+++ APIs/Insert/insert_user_data.php
@@ -1,86 +1,34 @@
 <?php
 
-include "D:\study\Projects\Kotlin\TripMate\APIs\connection.php";
+function insertUserData($con, $user_email, $password, $first_name, $last_name, $city_id, $profile_picture_id)
+{
+    $arr = [];
 
-$arr = [];
+    if (!$con) {
+        $arr["success"] = false;
+        $arr["error"] = "Connection failed";
+        return json_encode($arr);
+    }
 
-if ($con) {
-    $arr["connection"] = "Success";
-    echo json_encode($arr);
-} else {
-    $arr["connection"] = "Failed";
-    echo json_encode($arr);
-    return;
+    if (empty($user_email) || empty($password) || empty($first_name) || empty($last_name) || empty($city_id) || empty($profile_picture_id)) {
+        $arr["success"] = false;
+        $arr["error"] = "Invalid input";
+        return json_encode($arr);
+    }
+
+    $query = "CALL InsertTrip(?, ?, ?, ?, ?, ?, ?)";
+    $stmt = mysqli_prepare($con, $query);
+    $password = md5($password);
+    mysqli_stmt_bind_param($stmt, "sssssss", $user_email, $password, $first_name, $last_name, $city_id, $profile_picture_id);
+    mysqli_stmt_execute($stmt);
+
+    if (mysqli_stmt_affected_rows($stmt) > 0) {
+        $arr["success"] = true;
+        $arr["user_id"] = mysqli_insert_id($con);
+    } else {
+        $arr["success"] = false;
+        $arr["error"] = "Insertion failed";
+    }
+
+    return json_encode($arr);
 }
-
-if (isset($_POST["user_email"])) {
-    $user_email = $_POST["user_email"];
-} else {
-    $arr["success"] = "false";
-    $arr["error"] = "$user_email incorrect";
-    echo json_encode($arr);
-    return;
-}
-
-if (isset($_POST["password"])) {
-    $password = md5($_POST["password"]);
-} else {
-    $arr["success"] = "false";
-    $arr["error"] = "$password incorrect";
-    echo json_encode($arr);
-    return;
-}
-
-if (isset($_POST["first_name"])) {
-    $first_name = $_POST["first_name"];
-} else {
-    $arr["success"] = "false";
-    $arr["error"] = "$first_name incorrect";
-    echo json_encode($arr);
-    return;
-}
-
-if (isset($_POST["last_name"])) {
-    $last_name = $_POST["last_name"];
-} else {
-    $arr["success"] = "false";
-    $arr["error"] = "$last_name incorrect";
-    echo json_encode($arr);
-    return;
-}
-
-if (isset($_POST["city_id"])) {
-    $city_id = $_POST["city_id"];
-} else {
-    $arr["success"] = "false";
-    $arr["error"] = "$city_id incorrect";
-    echo json_encode($arr);
-    return;
-}
-
-if (isset($_POST["profile_picture_id"])) {
-    $profile_picture_id = $_POST["profile_picture_id"];
-} else {
-    $arr["success"] = "false";
-    $arr["error"] = "$profile_picture_id incorrect";
-    echo json_encode($arr);
-    return;
-}
-
-
-$query = "CALL InsertTrip(?, ?, ?, ?, ?, ?, ?)";
-
-$stmt = mysqli_prepare($con, $query);
-mysqli_stmt_bind_param($stmt, "sssssss", $user_email, $password, $first_name, $last_name, $city_id, $profile_picture_id);
-mysqli_stmt_execute($stmt);
-
-if (mysqli_stmt_affected_rows($stmt) > 0) {
-    $arr["success"] = "true";
-    $arr["user_id"] = mysqli_insert_id($con);
-} else {
-    $arr["success"] = "false";
-    $arr["error"] = "Insertion failed";
-}
-
-echo json_encode($arr);
-?>
