d989a694e44556d73da565089b05b48a78b8be21
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-30 17:41:51 +0500

    Feat: Edit insert_chat_data.php Fix connection error and update parameter handling

--- APIs/Insert/insert_chat_data.php
+++ APIs/Insert/insert_chat_data.php
@@ -1,27 +1,24 @@
 <?php
 
-include "D:\study\Projects\Kotlin\TripMate\APIs\connection.php";
+include "D:\\study\\Projects\\Kotlin\\TripMate\\APIs\\connection.php";
 
 $arr = [];
 
 $con = dbConnection();
 
-if ($con) {
-    $arr["connection"] = "Success";
-    echo json_encode($arr);
-}
-else{
+
+if ($con->connect_error) {
     $arr["connection"] = "Failed";
-    echo json_encode($arr);
-    return;
+    die("Connection failed: " . $con->connect_error);
 }
 
+
 // Validate and sanitize the input parameters
-$group_id = isset($_POST["group_id"]) ? $_POST["group_id"] : null;
-$user_id = isset($_POST["user_id"]) ? $_POST["user_id"] : null;
-$message = isset($_POST["message"]) ? $_POST["message"] : null;
-$image_id = isset($_POST["image_id"]) ? $_POST["image_id"] : null;
-$chat_time = isset($_POST["chat_time"]) ? $_POST["chat_time"] : null;
+$group_id = isset($_GET["group_id"]) ? $_GET["group_id"] : null;
+$user_id = isset($_GET["user_id"]) ? $_GET["user_id"] : null;
+$message = isset($_GET["message"]) ? $_GET["message"] : null;
+$image_id = isset($_GET["image_id"]) ? $_GET["image_id"] : null;
+$chat_time = isset($_GET["chat_time"]) ? $_GET["chat_time"] : null;
 
 // Check if any required parameter is missing
 if (!$group_id || !$user_id || !$message || !$image_id || !$chat_time) {
@@ -35,7 +32,7 @@ $query = "CALL InsertChat('$group_id', '$user_id', '$message', '$image_id', '$ch
 $res = mysqli_query($con, $query);
 if ($res) {
     $arr["success"] = "true";
-    $arr["chat_id"] = mysqli_insert_id($con);
+    $arr["chat_id"] = $con->insert_id;
 } else {
     $arr["success"] = "false";
 }
