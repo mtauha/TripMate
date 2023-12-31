dbe945fdef3163036057617c2aafc7a55471ffa1
mtauha <111292199+mtauha@users.noreply.github.com>
2023-12-31 17:44:13 +0500

    Add trip_id retrieval and update retrieveTripData()

--- APIs/Pages/all_trips.php
+++ APIs/Pages/all_trips.php
@@ -4,6 +4,7 @@ include "D:\\study\\Projects\\Kotlin\\TripMate\\APIs\\connection.php";
 include "D:\\study\\Projects\\Kotlin\\TripMate\\APIs\\Retrieve\\retrieve_trip_data.php";
 
 $arr = [];
+$user_id = $_GET["user_id"];
 
 $con = dbConnection();
 
@@ -12,7 +13,25 @@ if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
 
+$script_to_retrieve_trip_id =
+"SELECT trip_id FROM trip
+NATURAL JOIN `group`
+NATURAL JOIN user_to_group_relation
+WHERE user_id = '$user_id'
+";
 
-echo retrieveTripData($con);
 
+$trip_id_result = $con->query($script_to_retrieve_trip_id);
+if ($trip_id_result->num_rows > 0) {
+    while ($row = $trip_id_result->fetch_assoc()) {
+        $trip_id = $row["trip_id"];
+    }
+} else {
+    $arr["error"] = "Trip_id retrieval Failed";
+    die("Failed to retrieve trip_id: " . $con->connect_error);
+}
+
+echo json_encode(retrieveTripData($con, array("trip_id"=>$trip_id)), JSON_PRETTY_PRINT);
+
+mysqli_close($con);
 ?>
