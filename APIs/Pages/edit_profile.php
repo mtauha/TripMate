f872c170c28516de1cec16c1ffca27203f207a2f
mtauha <111292199+mtauha@users.noreply.github.com>
2024-01-02 03:43:51 +0500

    Refactor edit_profile.php To improve code readability and security

--- APIs/Pages/edit_profile.php
+++ APIs/Pages/edit_profile.php
@@ -5,6 +5,25 @@ include "D:\\study\\Projects\\Kotlin\\TripMate\\APIs\\Update\\update_user_data.p
 
 $arr = [];
 
+$user_id = isset($_GET["user_id"]) ? $_GET["user_id"] : 'null';
+$user_email = isset($_GET["user_email"]) ? "'".  $_GET["user_email"] . "'" : 'null';
+$phone_number = isset($_GET["phone_number"]) ? "'" . $_GET["phone_number"] . "'" : 'null';
+$date_of_birth = isset($_GET["date_of_birth"]) ? "'" . $_GET["date_of_birth"] . "'" : 'null';
+$first_name = isset($_GET["first_name"]) ? "'" . $_GET["first_name"] . "'" : 'null';
+$last_name = isset($_GET["last_name"]) ? "'" . $_GET["last_name"] . "'" : 'null';
+$date_of_birth = isset($_GET["date_of_birth"]) ? "'" . $_GET["date_of_birth"] . "'" : 'null';
+$phone_number = isset($_GET["phone_number"]) ? "'" . $_GET["phone_number"] . "'" : 'null';
+$city_name = isset($_GET["city_name"]) ? $_GET["city_name"] : null;
+$profile_pic_id = isset($_GET["profile_pic_id"]) ? $_GET["profile_pic_id"] : 'null';
+
+if(isset($_GET["password"])){
+    $password = "'" . md5($_GET["password"]) . "'";
+
+}
+else
+    $password = 'null';
+
+
 $con = dbConnection();
 
 if ($con->connect_error) {
@@ -12,23 +31,21 @@ if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
 
-$user_email = $_GET["user_email"];
+if($city_name){
+    $query_to_retrieve_cityid = "SELECT city_id FROM city WHERE city_name = $city_name";
+    $city_id_result = $con->query($query_to_retrieve_cityid);
+    $city_id = $city_id_result->fetch_assoc()["city_id"];
+} else
+    $city_id = 'null';
+    
 
-$query = "SELECT user_id FROM user WHERE user_email = '$user_email'";
-$result = mysqli_query($con, $query);
-if (mysqli_num_rows($result) > 0) {
-    $row = mysqli_fetch_assoc($result);
-    $user_id = $row["user_id"];
-} else {
-    $arr["success"] = false;
-    $arr["error"] = "User with user_email $user_email not found";
-    die(json_encode($arr));
-}
+// Assuming $password and $profile_pic_id are provided from somewhere
 
-$oldAttributes = $_GET['oldAttributes'];
-$newAttributes = $_GET['newAttributes'];
+// Add single quotes for string values in the SQL query
+$updateQuery = "CALL UpdateUser($user_id, $user_email, $phone_number, $date_of_birth, $password, $first_name, $last_name, $city_id, $profile_pic_id)";
 
-$result = editUserData($con, $user_id, $oldAttributes, $newAttributes);
+// Execute the UPDATE query
+$result = mysqli_query($con, $updateQuery);
 
 if ($result) {
     $arr["success"] = true;
