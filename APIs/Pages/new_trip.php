96284e0a9d3d7105aba564ff4cdb3817e0dd744a
mtauha <111292199+mtauha@users.noreply.github.com>
2024-01-01 16:47:57 +0500

    Update start_date variable in new_trip.php

--- APIs/Pages/new_trip.php
+++ APIs/Pages/new_trip.php
@@ -16,7 +16,7 @@ if ($con->connect_error) {
 $trip_name = $_GET["trip_name"] ?? "";
 $source_city = $_GET["source_city"] ?? "";
 $destination_city = $_GET["destination_city"] ?? "";
-$DATE = $_GET["DATE"] ?? "";
+$start_date = $_GET["start_date"] ?? "";
 $trip_days = $_GET["trip_days"] ?? "";
 $trip_budget = $_GET["trip_budget"] ?? "";
 $no_of_persons = $_GET["no_of_persons"] ?? "";
@@ -57,7 +57,7 @@ if ($result) {
     echo "Error: " . mysqli_error($con);
 }
 
+echo insertTripData($con, $trip_name, $source_city_id, $destination_city_id, (string)$start_date, $trip_days, $trip_budget, $no_of_persons, $admin_id);
 
-echo insertTripData($con, $trip_name, $source_city_id, $destination_city_id, $DATE, $trip_days, $trip_budget, $no_of_persons, $admin_id);
 
 ?>
