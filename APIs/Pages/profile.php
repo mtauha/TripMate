fc86ee3581b7132541011659a2d5df85210b20a1
mtauha <111292199+mtauha@users.noreply.github.com>
2024-01-02 02:44:13 +0500

    Refactor profile.php Add functions for replacing key and value, retrieving city name, and retrieving user data

--- APIs/Pages/profile.php
+++ APIs/Pages/profile.php
@@ -4,6 +4,30 @@ include "D:\\study\\Projects\\Kotlin\\TripMate\\APIs\\connection.php";
 include "D:\\study\\Projects\\Kotlin\\TripMate\\APIs\\Retrieve\\retrieve_user_data.php";
 include "D:\\study\\Projects\\Kotlin\\TripMate\\APIs\\Retrieve\\retrieve_image_data.php";
 
+function replaceKeyAndValue($array, $oldKey, $newKey, $newValue)
+{
+    if (array_key_exists($oldKey, $array)) {
+        unset($array[$oldKey]);
+        $array[$newKey] = $newValue;
+    }
+
+    return $array;
+}
+
+function getCityName($con, $cityId)
+{
+    $query = "SELECT city_name FROM city WHERE city_id = $cityId";
+    $result = mysqli_query($con, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        return $row['city_name'];
+    }
+
+    return null;
+}
+
+
 $arr = [];
 $user_email = $_GET["user_email"];
 
@@ -14,68 +38,59 @@ if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
 
-$user_id_query = "SELECT user_id FROM user WHERE user_email = '$user_email'";
-$result = mysqli_query($con, $user_id_query);
-if (mysqli_num_rows($result) > 0) {
+$query = "SELECT user_id FROM user WHERE user_email = '$user_email'";
+$result = mysqli_query($con, $query);
+if ($result && mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
-    $user_id = $row["user_id"];
+    $user_id = $row['user_id'];
 } else {
-    $arr["success"] = false;
-    $arr["error"] = "User with user_email $user_email not found";
-    die(json_encode($arr));
+    $arr["error"] = "User not found";
+    echo json_encode($arr);
+    return;
 }
-
 
 // Retrieve user data
-$userData = retrieveUserData($con, ["user_id" => $user_id]);
-$userData = json_decode($userData, true);  // Added true to decode as associative array
+$userData = json_decode(retrieveUserData($con, $_GET),true);
 
 
-$city_id = $userData["user_data"][0]["city_id"];
-$query_to_retrieve_cityname = "SELECT city_name FROM city WHERE city_id = $city_id";
-$result = mysqli_query($con, $query_to_retrieve_cityname);
-
-if ($result) {
-    $row = mysqli_fetch_assoc($result);
-    $city_name = $row['city_name'];
-} else {
-    echo "Error: " . mysqli_error($con);
-}
-
-
-// Check if user data exists
 if ($userData) {
-    // Retrieve image data using the profile_picture_id from user data
-    $im_id = $userData["user_data"][0]["profile_picture_id"];
+    $cityId = $userData["user_data"][0]["city_id"];
+    $cityName = getCityName($con, $cityId);
 
-    if ($im_id) {
-        $imageData["image_data"] = json_decode(retrieveImageData($con,
-            [
-                "image_id" => $im_id
-            ],
-            [
-                "image_data"
-            ]
-        ), true);  // Added true to decode as associative array
+    if ($cityName) {
+        $userData["user_data"] = replaceKeyAndValue($userData["user_data"][0], "city_id", "city_name", $cityName);
 
-        $user = [
-            "data" => $userData["user_data"],
-            "profile_pic" => $imageData["image_data"]
-        ];
+        // Retrieve image data using the profile_picture_id from user data
+        $imageId = $userData["user_data"]["profile_picture_id"];
 
-        echo json_encode($user, JSON_PRETTY_PRINT);
+        if ($imageId) {
+            $imageData = json_decode(retrieveImageData($con, ["image_id" => $imageId], ["image_data"]), true);
+
+            $user = [
+                "data" => $userData["user_data"],
+                "profile_pic" => $imageData["image_data"]
+            ];
+
+            echo json_encode($user, JSON_PRETTY_PRINT);
+        } else {
+            // Handle the case when profile picture data is not found
+            $user = [
+                "data" => $userData["user_data"],
+                "profile_pic" => null
+            ];
+
+            echo json_encode($user, JSON_PRETTY_PRINT);
+        }
     } else {
-        // Handle the case when profile picture data is not found
-        $user = [
-            "data" => $userData["user_data"],
-            "profile_pic" => null
-        ];
-
-        echo json_encode($user, JSON_PRETTY_PRINT);
+        // Handle the case when city name is not found
+        $arr["error"] = "City name not found";
+        echo json_encode($arr);
     }
 } else {
     // Handle the case when user data is not found
     $arr["error"] = "User not found";
     echo json_encode($arr);
 }
+
+mysqli_close($con);
 ?>
