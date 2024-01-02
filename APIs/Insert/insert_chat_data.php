412d1a5937ec16b54ea241ca62ca085157a35a0d
mtauha <111292199+mtauha@users.noreply.github.com>
2024-01-02 08:54:27 +0500

    Refactor insert_chat_data.php To validate and sanitize input parameters

--- APIs/Insert/insert_chat_data.php
+++ APIs/Insert/insert_chat_data.php
@@ -14,20 +14,55 @@ if ($con->connect_error) {
 
 
 // Validate and sanitize the input parameters
-$group_id = isset($_GET["group_id"]) ? $_GET["group_id"] : null;
+$trip_id = isset($_GET["trip_id"]) ? $_GET["trip_id"] : null;
 $user_id = isset($_GET["user_id"]) ? $_GET["user_id"] : null;
 $message = isset($_GET["message"]) ? $_GET["message"] : null;
 $image_id = isset($_GET["image_id"]) ? $_GET["image_id"] : null;
 $chat_time = isset($_GET["chat_time"]) ? $_GET["chat_time"] : null;
 
+
 // Check if any required parameter is missing
-if (!$group_id || !$user_id || !$message || !$image_id || !$chat_time) {
+if (!$user_id) {
     $arr["success"] = "false";
+    $arr["message"] = "Missing required parameter user_id";
+    echo json_encode($arr);
+    return;
+
+}
+if (!$trip_id) {
+    $arr["success"] = "false";
+    $arr["message"] = "Missing required parameter trip_id";
+    echo json_encode($arr);
+    return;
+
+}
+if (!$message) {
+    $arr["success"] = "false";
+    $arr["message"] = "Missing required parameter message";
+    echo json_encode($arr);
+    return;
+
+}
+if (!$chat_time) {
+    $arr["success"] = "false";
+    $arr["message"] = "Missing required parameter chat_time";
     echo json_encode($arr);
     return;
 }
 
-$query = "CALL InsertChat('$group_id', '$user_id', '$message', '$image_id', '$chat_time')";
+
+$query =
+    "SELECT group_id FROM trip
+ NATURAL JOIN `group`
+WHERE trip_id = $trip_id ;";
+
+$result = $con->query($query);
+$row = $result->fetch_assoc();
+$group_id = $row["group_id"];
+
+
+
+$query = "CALL InsertChat($group_id, $user_id, '$message', $image_id, '$chat_time')";
 
 $res = mysqli_query($con, $query);
 if ($res) {
