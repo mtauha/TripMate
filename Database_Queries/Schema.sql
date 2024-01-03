1a183477ebff618f9c9c2ec339e9e75ce46d2057
mtauha <111292199+mtauha@users.noreply.github.com>
2024-01-03 18:01:56 +0500

    Refactor code to improve performance and readability

--- Database_Queries/Schema.sql
+++ Database_Queries/Schema.sql
@@ -1,73 +1,155 @@
 DROP DATABASE IF EXISTS TripDB;
-CREATE DATABASE TripDB;
+CREATE DATABASE `tripdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE TripDB;
 
-CREATE TABLE `country`(
-	country_id INT AUTO_INCREMENT,
-    country_name VARCHAR(50),
-    
-    CONSTRAINT country_country_id_constraint
-    PRIMARY KEY(country_id)
-);
+CREATE TABLE `budget` (
+  `budget_id` int NOT NULL AUTO_INCREMENT,
+  `trip_id` int DEFAULT NULL,
+  `reason` varchar(150) DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  PRIMARY KEY (`budget_id`),
+  KEY `trip_id` (`trip_id`),
+  CONSTRAINT `budget_ibfk_1` FOREIGN KEY (`trip_id`) REFERENCES `trip` (`trip_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `city`(
-	city_id INT AUTO_INCREMENT,
-    city_name VARCHAR(60),
-    country_id INT,
-    
-    CONSTRAINT city_city_id_constraint
-    PRIMARY KEY(city_id),
-    
-    CONSTRAINT city_country_id_constraint
-    FOREIGN KEY (country_id) REFERENCES country (country_id)
-);
 
-CREATE TABLE `location`(
-	location_id INT AUTO_INCREMENT,
-    city_id INT,
-    location_name VARCHAR(75),
-    location_type ENUM('TOURIST SPOT', 'HOTEL', 'RESTURAUNT'),
-    
-    CONSTRAINT location_location_id_constraint
-    PRIMARY KEY (location_id),
-    
-    CONSTRAINT location_city_id_constraint
-    FOREIGN KEY (city_id) REFERENCES city (city_id)
-);
+CREATE TABLE `chat` (
+  `chat_id` int NOT NULL AUTO_INCREMENT,
+  `group_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `message` varchar(500) NOT NULL,
+  `image_id` int DEFAULT NULL,
+  `chat_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`chat_id`),
+  KEY `chat_user_id_constraint` (`user_id`),
+  KEY `chat_group_id_constraint` (`group_id`),
+  KEY `group_image_id_constraint` (`image_id`),
+  CONSTRAINT `chat_group_id_constraint` FOREIGN KEY (`group_id`) REFERENCES `group` (`group_id`),
+  CONSTRAINT `chat_user_id_constraint` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `group_image_id_constraint` FOREIGN KEY (`image_id`) REFERENCES `image` (`image_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `trip`(
-	trip_id INT AUTO_INCREMENT,
-    trip_name VARCHAR(50),
-    source_city_id INT,
-    destination_city_id INT,
-    start_date DATE,
-    trip_days INT,
-    trip_budget INT,
-    no_of_persons INT,
-    
-    CONSTRAINT trip_trip_id_constraint 
-    PRIMARY KEY(trip_id),
-    
-    CONSTRAINT trip_source_city_id_constraint
-    FOREIGN KEY (source_city_id) REFERENCES city (city_id),
-    
-	CONSTRAINT trip_destination_city_id_constraint
-    FOREIGN KEY (destination_city_id) REFERENCES city (city_id)
-);
 
-CREATE TABLE `image`(
-	image_id INT AUTO_INCREMENT,
-    image_data BLOB,
-    image_type ENUM('LOCATION', 'CHAT', 'PROFILE PICTURE'),
-    
-	CONSTRAINT image_image_id_constraint
-    PRIMARY KEY (image_id)
-);
+CREATE TABLE `city` (
+  `city_id` int NOT NULL AUTO_INCREMENT,
+  `city_name` varchar(60) DEFAULT NULL,
+  `country_id` int DEFAULT NULL,
+  PRIMARY KEY (`city_id`),
+  KEY `city_country_id_constraint` (`country_id`),
+  CONSTRAINT `city_country_id_constraint` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=43671 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `country` (
+  `country_id` int NOT NULL AUTO_INCREMENT,
+  `country_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`country_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=251 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `group` (
+  `group_id` int NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(40) DEFAULT NULL,
+  `trip_id` int DEFAULT NULL,
+  PRIMARY KEY (`group_id`),
+  KEY `group_trip_id_constraint` (`trip_id`),
+  CONSTRAINT `group_trip_id_constraint` FOREIGN KEY (`trip_id`) REFERENCES `trip` (`trip_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `image` (
+  `image_id` int NOT NULL AUTO_INCREMENT,
+  `image_data` longblob,
+  `image_type` enum('LOCATION','CHAT','PROFILE PICTURE') DEFAULT NULL,
+  PRIMARY KEY (`image_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `location` (
+  `location_id` int NOT NULL AUTO_INCREMENT,
+  `city_id` int DEFAULT NULL,
+  `location_name` varchar(75) DEFAULT NULL,
+  `location_type` enum('TOURIST SPOT','HOTEL','RESTURAUNT') DEFAULT NULL,
+  PRIMARY KEY (`location_id`),
+  KEY `location_city_id_constraint` (`city_id`),
+  CONSTRAINT `location_city_id_constraint` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `notes` (
+  `note_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `trip_id` int DEFAULT NULL,
+  `body` varchar(1000) DEFAULT NULL,
+  `heading` varchar(85) DEFAULT NULL,
+  `note_date` date DEFAULT NULL,
+  PRIMARY KEY (`note_id`),
+  KEY `user_id` (`user_id`),
+  KEY `trip_id` (`trip_id`),
+  CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `notes_ibfk_2` FOREIGN KEY (`trip_id`) REFERENCES `trip` (`trip_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `notification` (
+  `notification_id` int NOT NULL AUTO_INCREMENT,
+  `notification_text` varchar(200) DEFAULT NULL,
+  `read_status` tinyint(1) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `group_id` int DEFAULT NULL,
+  PRIMARY KEY (`notification_id`),
+  KEY `notification_user_id_constraint` (`user_id`),
+  KEY `notification_group_id_constraint` (`group_id`),
+  CONSTRAINT `notification_group_id_constraint` FOREIGN KEY (`group_id`) REFERENCES `group` (`group_id`),
+  CONSTRAINT `notification_user_id_constraint` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `picture_to_location_relation` (
+  `location_id` int NOT NULL,
+  `image_id` int NOT NULL,
+  PRIMARY KEY (`location_id`,`image_id`),
+  KEY `relation_image_id_constraint` (`image_id`),
+  CONSTRAINT `relation_image_id_constraint` FOREIGN KEY (`image_id`) REFERENCES `image` (`image_id`),
+  CONSTRAINT `relation_location_id_constraint` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `review` (
+  `review_id` int NOT NULL,
+  `review_text` varchar(200) NOT NULL,
+  `review_stars` enum('1','2','3','4','5') DEFAULT NULL,
+  `location_id` int DEFAULT NULL,
+  PRIMARY KEY (`review_id`),
+  KEY `review_location_id_constraint` (`location_id`),
+  CONSTRAINT `review_location_id_constraint` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `trip` (
+  `trip_id` int NOT NULL AUTO_INCREMENT,
+  `trip_name` varchar(50) DEFAULT NULL,
+  `source_city_id` int DEFAULT NULL,
+  `destination_city_id` int DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `trip_days` int DEFAULT NULL,
+  `trip_budget` int DEFAULT NULL,
+  `no_of_persons` int DEFAULT NULL,
+  `admin_id` int DEFAULT NULL,
+  PRIMARY KEY (`trip_id`),
+  KEY `trip_source_city_id_constraint` (`source_city_id`),
+  KEY `trip_destination_city_id_constraint` (`destination_city_id`),
+  KEY `group_admin_id_constraint` (`admin_id`),
+  CONSTRAINT `group_admin_id_constraint` FOREIGN KEY (`admin_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `trip_destination_city_id_constraint` FOREIGN KEY (`destination_city_id`) REFERENCES `city` (`city_id`),
+  CONSTRAINT `trip_source_city_id_constraint` FOREIGN KEY (`source_city_id`) REFERENCES `city` (`city_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE `user` (
   `user_id` int NOT NULL AUTO_INCREMENT,
   `date_of_birth` date DEFAULT NULL,
-  `phone_number` int DEFAULT NULL,
+  `phone_number` varchar(11) DEFAULT NULL,
   `user_email` varchar(40) DEFAULT NULL,
   `password` varchar(35) DEFAULT NULL,
   `first_name` varchar(20) DEFAULT NULL,
@@ -77,95 +159,14 @@ CREATE TABLE `user` (
   PRIMARY KEY (`user_id`),
   KEY `profile_picture_id` (`profile_picture_id`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`profile_picture_id`) REFERENCES `image` (`image_id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `group`(
-	group_id INT AUTO_INCREMENT,
-    group_name VARCHAR(40),
-    trip_id INT,
-    
-    CONSTRAINT group_group_id_constraint
-    PRIMARY KEY (group_id),
-    
-    CONSTRAINT group_trip_id_constraint
-    FOREIGN KEY (trip_id) REFERENCES trip (trip_id)
-);
 
-CREATE TABLE `user_to_group_relation`(
-	user_id INT,
-    group_id INT,
-    
-    CONSTRAINT user_group_composite_constraint
-    PRIMARY KEY (user_id, group_id),
-    
-    CONSTRAINT relation_user_id_constraint
-    FOREIGN KEY (user_id) REFERENCES `user` (user_id),
-    
-	CONSTRAINT relation_group_id_constraint
-    FOREIGN KEY (group_id) REFERENCES `group` (group_id)
-);
-
-CREATE TABLE `picture_to_location_relation`(
-	location_id INT,
-    image_id INT,
-
-	CONSTRAINT picture_location_composite_constraint
-    PRIMARY KEY (location_id, image_id),
-    
-    CONSTRAINT relation_location_id_constraint
-    FOREIGN KEY (location_id) REFERENCES location (location_id),
-    
-    CONSTRAINT relation_image_id_constraint
-    FOREIGN KEY (image_id) REFERENCES image (image_id)
-);
-
-CREATE TABLE `chat`(
-	chat_id INT AUTO_INCREMENT,
-    group_id INT,
-    user_id INT,
-    message VARCHAR(500) NOT NULL,
-    image_id INT,
-    chat_time TIME,
-    
-    CONSTRAINT chat_chat_id_constraint
-    PRIMARY KEY (chat_id),
-
-	CONSTRAINT chat_user_id_constraint
-    FOREIGN KEY (`user_id`) REFERENCES `user` (user_id), 
-    
-    CONSTRAINT chat_group_id_constraint
-    FOREIGN KEY (`group_id`) REFERENCES `group` (group_id),
-    
-	CONSTRAINT group_image_id_constraint
-    FOREIGN KEY (image_id) REFERENCES image (image_id)
-);
-
-CREATE TABLE `notification`(
-	notification_id INT AUTO_INCREMENT,
-    notification_text VARCHAR(200),
-    read_status BOOLEAN,
-    user_id INT,
-    group_id INT,
-    
-    CONSTRAINT notification_notification_id_constraint 
-    PRIMARY KEY(notification_id),
-    
-    CONSTRAINT notification_user_id_constraint
-    FOREIGN KEY (user_id) REFERENCES `user` (user_id),
-    
-	CONSTRAINT notification_group_id_constraint
-    FOREIGN KEY (group_id) REFERENCES `group` (group_id)
-);
-
-CREATE TABLE review(
-	review_id INT,
-    review_text VARCHAR(200) NOT NULL,
-    review_stars ENUM('1', '2', '3', '4', '5'),
-    location_id INT,
-    
-    CONSTRAINT review_review_id_constraint
-    PRIMARY KEY (review_id),
-    
-    CONSTRAINT review_location_id_constraint
-    FOREIGN KEY (location_id) REFERENCES location (location_id)
-);
+CREATE TABLE `user_to_group_relation` (
+  `user_id` int NOT NULL,
+  `group_id` int NOT NULL,
+  PRIMARY KEY (`user_id`,`group_id`),
+  KEY `relation_group_id_constraint` (`group_id`),
+  CONSTRAINT `relation_group_id_constraint` FOREIGN KEY (`group_id`) REFERENCES `group` (`group_id`),
+  CONSTRAINT `relation_user_id_constraint` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
